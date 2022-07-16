2a39e1c19ec0686a55e8d854e6be3a320668d57a
Sergey Karavelkov <s.karavelkov@gmail.com>
2022-07-16 18:34:29 +0300

    Initialized project

---
+++ src/test/java/net/absoft/mortalwombat/AgeCheckerTest.java
@@ -0,0 +1,18 @@
+package net.absoft.mortalwombat;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AgeCheckerTest {
+
+    @Test
+    public void testAgedUserCanPlay() {
+        AgeChecker ageChecker = new AgeChecker();
+        Assert.assertTrue(ageChecker.canPlayGame(19), "Aged user can't play game");
+    }
+    @Test
+    public void testThatTooYongUsersCanNotPlay() {
+        AgeChecker ageChecker = new AgeChecker();
+        Assert.assertFalse(ageChecker.canPlayGame(17), "Under aged user can play game");
+    }
+}
